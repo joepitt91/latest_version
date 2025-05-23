9c67d95c1266606f81e654876dee3b186ca24605
joepitt91 <16159859+joepitt91@users.noreply.github.com>
2025-05-23 16:58:51 +0100

    fix: gchr tag access & github API pagination

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ SPDX-License-Identifier: GPL-3.0-only
 -->
 # Changelog
 
+## v1.0.2
+
+* fix: github container registry tag access
+* fix: github API pagination
+
 ## v1.0.1
 
 * fix source directory
--- src/get_latest_version/github.py
+++ src/get_latest_version/github.py
@@ -4,7 +4,7 @@
 
 """Get latest version information from GitHub packages and repositories."""
 
-from typing import Dict, Literal, Optional, Union
+from typing import Any, Dict, List, Literal, Optional
 
 from semver import Version
 from requests import get
@@ -24,7 +24,7 @@ def get_latest_version_from_package(  # pylint: disable=too-many-arguments
     minimum_version: Optional[Version] = None,
     maximum_version: Optional[Version] = None,
 ) -> str:
-    """Get the latest version from a package.
+    """Get the latest version from a GitHub package.
 
     Args:
         token (str): The token to authenticate to GitHub with.
@@ -46,28 +46,55 @@ def get_latest_version_from_package(  # pylint: disable=too-many-arguments
     """
 
     semantic_versions: Dict[str, Version] = {}
-    response = get(
-        f"https://api.github.com/users/{owner}/packages/{package_type}/{package_name}/versions",
-        headers={
-            "Authorization": f"Bearer {token}",
-            "User-Agent": f"Python get_latest_version/v{__version__}",
-        },
-        timeout=10,
-    )
-    response.raise_for_status()
 
-    versions: Dict[str, Union[str, Dict[str, str]]] = response.json()
-    for version in versions:
-        try:
-            semantic_version = Version.parse(clean_version(version["name"]))
-            if (minimum_version is not None and semantic_version < minimum_version) or (
-                maximum_version is not None and semantic_version > maximum_version
-            ):
-                continue
-            semantic_versions[version["name"]] = semantic_version
-        except (TypeError, ValueError):
-            continue
+    page = 1
+    while True:  # pylint: disable=too-many-nested-blocks
+        response = get(
+            f"https://api.github.com/users/{owner}/packages/{package_type}/{package_name}/versions"
+            f"?per_page=100&page={page}",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "User-Agent": f"Python get_latest_version/v{__version__}",
+            },
+            timeout=10,
+        )
+        response.raise_for_status()
+        versions: List[Dict[str, Any]] = response.json()
 
+        if len(versions) == 0:
+            break
+        page = page + 1
+
+        for version in versions:
+            if package_type == "container":
+                for tag in version["metadata"]["container"]["tags"]:
+                    try:
+                        semantic_version = Version.parse(clean_version(tag))
+                        if (
+                            minimum_version is not None
+                            and semantic_version < minimum_version
+                        ) or (
+                            maximum_version is not None
+                            and semantic_version > maximum_version
+                        ):
+                            continue
+                        semantic_versions[tag] = semantic_version
+                    except (TypeError, ValueError):
+                        continue
+            else:
+                try:
+                    semantic_version = Version.parse(clean_version(version["name"]))
+                    if (
+                        minimum_version is not None
+                        and semantic_version < minimum_version
+                    ) or (
+                        maximum_version is not None
+                        and semantic_version > maximum_version
+                    ):
+                        continue
+                    semantic_versions[version["name"]] = semantic_version
+                except (TypeError, ValueError):
+                    continue
     return find_latest(semantic_versions)
 
 
@@ -99,32 +126,39 @@ def get_latest_version_from_releases(
     """
 
     semantic_versions: Dict[str, Version] = {}
-    response = get(
-        f"https://api.github.com/repos/{owner}/{repository}/releases",
-        headers={
-            "Authorization": f"Bearer {token}",
-            "User-Agent": f"Python get_latest_version/v{__version__}",
-        },
-        timeout=10,
-    )
-    response.raise_for_status()
 
-    releases: Dict[str, Union[str, Dict[str, str]]] = response.json()
-    for release in releases:
-        if release["draft"]:
-            continue
-        try:
-            semantic_version = Version.parse(clean_version(release["name"]))
-        except (TypeError, ValueError):
-            try:
-                semantic_version = Version.parse(clean_version(release["tag_name"]))
-            except (TypeError, ValueError):
+    page = 1
+    while True:
+        response = get(
+            f"https://api.github.com/repos/{owner}/{repository}/releases?per_page=100&page={page}",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "User-Agent": f"Python get_latest_version/v{__version__}",
+            },
+            timeout=10,
+        )
+        response.raise_for_status()
+        releases: List[Dict[str, Any]] = response.json()
+
+        if len(releases) == 0:
+            break
+        page = page + 1
+
+        for release in releases:
+            if release["draft"]:
                 continue
-        if (minimum_version is not None and semantic_version < minimum_version) or (
-            maximum_version is not None and semantic_version > maximum_version
-        ):
-            continue
-        semantic_versions[release["name"]] = semantic_version
+            try:
+                semantic_version = Version.parse(clean_version(release["name"]))
+            except (TypeError, ValueError):
+                try:
+                    semantic_version = Version.parse(clean_version(release["tag_name"]))
+                except (TypeError, ValueError):
+                    continue
+            if (minimum_version is not None and semantic_version < minimum_version) or (
+                maximum_version is not None and semantic_version > maximum_version
+            ):
+                continue
+            semantic_versions[release["name"]] = semantic_version
 
     return find_latest(semantic_versions)
 
@@ -157,26 +191,35 @@ def get_latest_version_from_tags(
     """
 
     semantic_versions: Dict[str, Version] = {}
-    response = get(
-        f"https://api.github.com/repos/{owner}/{repository}/tags",
-        headers={
-            "Authorization": f"Bearer {token}",
-            "User-Agent": f"Python get_latest_version/v{__version__}",
-        },
-        timeout=10,
-    )
-    response.raise_for_status()
 
-    tags: Dict[str, Union[str, Dict[str, str]]] = response.json()
-    for tag in tags:
-        try:
-            semantic_version = Version.parse(clean_version(tag["name"]))
-            if (minimum_version is not None and semantic_version < minimum_version) or (
-                maximum_version is not None and semantic_version > maximum_version
-            ):
-                continue
-            semantic_versions[tag["name"]] = semantic_version
-        except (TypeError, ValueError):
-            pass
+    page = 1
+    while True:
+        response = get(
+            f"https://api.github.com/repos/{owner}/{repository}/tags?per_page=100&page={page}",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "User-Agent": f"Python get_latest_version/v{__version__}",
+            },
+            timeout=10,
+        )
+        response.raise_for_status()
+        tags: List[Dict[str, Any]] = response.json()
+
+        if len(tags) == 0:
+            break
+        page = page + 1
+
+        for tag in tags:
+            try:
+                semantic_version = Version.parse(clean_version(tag["name"]))
+                if (
+                    minimum_version is not None and semantic_version < minimum_version
+                ) or (
+                    maximum_version is not None and semantic_version > maximum_version
+                ):
+                    continue
+                semantic_versions[tag["name"]] = semantic_version
+            except (TypeError, ValueError):
+                pass
 
     return find_latest(semantic_versions)
